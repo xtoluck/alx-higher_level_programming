d58007a676ce518de4f7f583fc3489f6c678438f
Lucky <xtoluck.com>
2023-11-30 13:18:50 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from variable_load_5 import x
-    print(x)
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
