8bbfaa097d21e7c29ed27d7a6cfce3adf050776e
Lucky <xtoluck.com>
2023-11-30 13:02:56 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from calculator_1 import add, sub, mult, div
-    x = 10
-    y = 5
-    print("{} + {} = {}".format(x, y, add(x, y)))
-    print("{} - {} = {}".format(x, y, sub(x, y)))
-    print("{} * {} = {}".format(x, y, mult(x, y)))
-    print("{} / {} = {}".format(x, y, div(x, y)))
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
